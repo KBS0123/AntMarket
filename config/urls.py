a7247fd226bf72faf0f79af23b68beb6caa1de5c
JOJOJOJO27 <jinjo802828@gmail.com>
2024-06-05 14:20:04 +0900

    임시 세이브

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('chat/', include('chat.urls')),  # chat 앱의 URL 패턴을 포함
     path('', include('market.urls', namespace='market')),
+    path('chat/', include('chat.urls')),
 ]
