97a3885493df2ccd6d678cfd8cbc4a3abe73748c
JOJOJOJO27 <jinjo802828@gmail.com>
2024-06-05 16:23:44 +0900

    해치웠나?

--- chat/models.py
+++ chat/models.py
@@ -2,14 +2,15 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class ChatRoom(models.Model):
     # 채팅방에 대한 필드 추가 (예: 이름, 생성일 등)
     name = models.CharField(default=None, max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
 class Message(models.Model):
     room = models.ForeignKey(ChatRoom, related_name='messages', on_delete=models.CASCADE, default=None)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(max_length=255, default='')
+    timestamp = models.DateTimeField(default=timezone.now)
