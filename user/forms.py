0939b940a736866d68185c764c32bc4ee62f4045
KBS0123 <s3260626@gmail.com>
2024-06-10 17:43:17 +0900

    프로필 페이지 추가

--- user/forms.py
+++ user/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    profile_image = forms.ImageField(label="프로필 이미지", required=False)
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2", "email")
+        fields = ("username", "password1", "password2", "email", "profile_image")
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from user.forms import UserForm
+from .models import UserProfile
 
 def logout_view(request):
     logout(request)
@@ -10,7 +11,11 @@ def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            profile_image = form.cleaned_data.get('profile_image')
+            user_profile = UserProfile(user=user, profile_image=profile_image)
+            user_profile.save()
+
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
@@ -20,5 +25,10 @@ def signup(request):
         form = UserForm()
     return render(request, 'user/signup.html', {'form': form})
 
+def profile(request):
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+    except UserProfile.DoesNotExist:
+        user_profile = None
 
-
+    return render(request, 'user/profile.html', {'user_profile': user_profile})
