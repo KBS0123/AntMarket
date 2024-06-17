fc22f6cc0ce208bba026942315bbb48b6abb7c17
ChopilSung <whvlftjd90@gmail.com>
2024-06-17 18:05:28 +0900

    지도 api 수정

--- market/models.py
+++ market/models.py
@@ -49,6 +49,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=0)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
