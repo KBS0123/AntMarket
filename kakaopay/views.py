93da136eba69ed68e89ceb6a66033226d7a3e29b
KBS0123 <s3260626@gmail.com>
2024-06-18 10:41:10 +0900

    카카오페이 결제 수정 완

--- kakaopay/views.py
+++ kakaopay/views.py
@@ -1,14 +1,18 @@
 # kakaopay/views.py
 from django.shortcuts import render, redirect
-from django.urls import reverse
 import requests
-from django.conf import settings
-from .models import KakaoPayment
 from django.http import HttpResponse
+from cart.cart import Cart
 
-# Create your views here.
+next_order_id = 1001
 def index(request):
+    global next_order_id
+
     if request.method == "POST":
+        item_name = request.POST.get('item_name')
+        quantity = request.POST.get('quantity')
+        total_amount = request.POST.get('total_amount')
+
         url = 'https://kapi.kakao.com/v1/payment/ready'
         headers = {
             'Authorization': 'KakaoAK 650da9554612f198b916502e78c1aad4',
@@ -16,11 +20,11 @@ def index(request):
         }
         data = {
             "cid": "TC0ONETIME",    # 테스트용 코드
-            "partner_order_id": "1001",     # 주문번호
-            "partner_user_id": "german",    # 유저 아이디
-            "item_name": "연어초밥",        # 구매 물품 이름
-            "quantity": "1",                # 구매 물품 수량
-            "total_amount": "12000",        # 구매 물품 가격
+            "partner_order_id": str(next_order_id),     # 주문번호
+            "partner_user_id": request.user.username,    # 유저 아이디
+            "item_name": item_name,        # 구매 물품 이름
+            "quantity": quantity,                # 구매 물품 수량
+            "total_amount": total_amount,        # 구매 물품 가격
             "tax_free_amount": "0",         # 구매 물품 비과세
             "approval_url": "http://127.0.0.1:8000/kakaopay/success/",
             "cancel_url": "http://127.0.0.1:8000/kakaopay/cancel/",
@@ -30,11 +34,16 @@ def index(request):
         res = requests.post(url, headers=headers, data=data)
         request.session['tid'] = res.json()['tid']      # 결제 승인시 사용할 tid를 세션에 저장
         next_url = res.json()['next_redirect_pc_url']   # 결제 페이지로 넘어갈 url을 저장
+
+        next_order_id += 1
+
         return redirect(next_url)
 
-    return render(request, 'kakaopay/index.html')
+    return HttpResponse(status=405)
 
 def success(request):
+    global next_order_id
+
     url = 'https://kapi.kakao.com/v1/payment/approve'
     headers = {
         'Authorization': 'KakaoAK 650da9554612f198b916502e78c1aad4',
@@ -43,18 +52,22 @@ def success(request):
     params = {
         "cid": "TC0ONETIME",  # 테스트용 코드
         "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
-        "partner_order_id": "1001",  # 주문번호
-        "partner_user_id": "german",  # 유저 아이디
+        "partner_order_id": str(next_order_id - 1),  # 주문번호
+        "partner_user_id": request.user.username,  # 유저 아이디
         "pg_token": request.GET.get("pg_token"),  # 쿼리 스트링으로 받은 pg토큰
     }
 
     res = requests.post(url, headers=headers, params=params)
     amount = res.json()['amount']['total']
+
     res = res.json()
     context = {
         'res': res,
         'amount': amount,
     }
+
+    cart = Cart(request)
+    cart.clear()
 
     return render(request, 'kakaopay/success.html', context)
 
