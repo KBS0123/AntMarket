4d9ba99860e93e7612d9d66fe8864132a177943e
ChopilSung <whvlftjd90@gmail.com>
2024-06-18 15:12:32 +0900

    회원탈퇴 기능 추가

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from market.models import Category, Product
