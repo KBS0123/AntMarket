562e48f6fffc9f27add44fd4788addf1ecf4920a
JOJOJOJO27 <jinjo802828@gmail.com>
2024-06-18 17:36:55 +0900

    채팅 완

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,6 @@ app_name = 'chat'
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    path('<str:room_name>/<int:id>/', views.room, name='room'),
+    path('<str:room_name>/<int:seller_id>_<int:user_id>/', views.room, name='room'),
 ]
 
