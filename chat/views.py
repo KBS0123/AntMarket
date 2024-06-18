4f900f13218bf2c732d8a9b51008ef827bc50029
JOJOJOJO27 <jinjo802828@gmail.com>
2024-06-18 15:50:43 +0900

    채팅 수정 수정 수정

--- chat/views.py
+++ chat/views.py
@@ -1,30 +1,29 @@
-# chat/views.py
-
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from market.models import Product
-
 from .models import Message
 from .forms import MessageForm
 
 def room(request, room_name, id):
+    product = get_object_or_404(Product, id=id)
+
     if request.method == 'POST':
         form = MessageForm(request.POST, request.FILES)
         if form.is_valid():
             message = form.save(commit=False)
             message.room_name = room_name
-            message.user = request.user  # 현재 사용자 설정
+            message.user = request.user
+            message.product = product
             message.save()
             return JsonResponse({'image_url': message.image.url}, status=201)
         else:
             return JsonResponse({'error': 'Invalid form'}, status=400)
 
-    product = get_object_or_404(Product, id=id)
     messages = Message.objects.filter(product=product.id).order_by('timestamp')
 
     return render(request, 'chat/room.html', {
         'room_name': room_name,
-
+        'product': product,
         'messages': messages,
         'form': MessageForm(initial={'room_name': room_name}),
     })
