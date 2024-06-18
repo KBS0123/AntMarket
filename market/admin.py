02e528c23660fc023182cc7fcac9be388e8a1936
JOJOJOJO27 <jinjo802828@gmail.com>
2024-06-18 12:46:38 +0900

    채팅 수정 수정

--- market/admin.py
+++ market/admin.py
@@ -15,7 +15,7 @@ class MiniCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_display = ['id', 'name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
